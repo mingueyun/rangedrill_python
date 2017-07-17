2fca955b7932909ce4df1e16907307d3d84c53ba
mingueyun <mingueyun@gmail.com>
2017-07-16 19:05:08 -0700

    Add files via upload

---
+++ range.py
@@ -0,0 +1,14 @@
+
+mins = [1]
+mins_len = len(mins)
+for i in range(0,4,1):
+    print(i)
+    
+mins = [1,2,3]
+mins_len = len(mins)
+for i in range(3,-1,-1):
+    print(i)
+mins = [1,2,3]
+mins_len = len(mins)
+for i in range(8,1,-2):
+    print(i)
